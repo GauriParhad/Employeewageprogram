96b5cadbdcae6018a6b1123f561ca29af2ff3247
Gaurip10 <thegauriparhad@gmail.com>
2021-08-10 19:16:11 +0530

    Added: Calculating Emp wage

--- src/empwagecomputation.java
+++ src/empwagecomputation.java
@@ -4,11 +4,20 @@ public class empwagecomputation {
 
         //constants
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+
+        //variables
+        int emphrs=0;
+        int empwage=0;
+
         //Computation
         double empcheck =Math.floor(Math.random() * 10)%2;
         if (empcheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+            emphrs=8;
         else
-            System.out.println("Employee is Absent");
+            emphrs=0;
+        empwage= emphrs * EMP_RATE_PER_HOUR
+            System.out.println("Employee wage:"+empwage);
+
     }
 }
