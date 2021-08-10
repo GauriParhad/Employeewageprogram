08c1c9a09f5d94861580cd2a097676a79a106c01
Gaurip10 <thegauriparhad@gmail.com>
2021-08-10 19:21:18 +0530

    Added: part tiem employee

--- src/empwagecomputation.java
+++ src/empwagecomputation.java
@@ -3,17 +3,20 @@ public class empwagecomputation {
         System.out.println("Welcome to Employee wage computation program");
 
         //constants
-        int IS_FULL_TIME=1;
+        int IS_FULL_TIME=2;
         int EMP_RATE_PER_HOUR=20;
+        int IS_PART_TIME=1;
 
         //variables
         int emphrs=0;
         int empwage=0;
 
         //Computation
-        double empcheck =Math.floor(Math.random() * 10)%2;
-        if (empcheck == IS_FULL_TIME)
-            emphrs=8;
+        double empcheck =Math.floor(Math.random() * 10) % 3;
+        if (empcheck == IS_PART_TIME)
+            emphrs=4;
+        else if (empcheck == IS_FULL_TIME)
+             emphrs=8;
         else
             emphrs=0;
         empwage= emphrs * EMP_RATE_PER_HOUR
